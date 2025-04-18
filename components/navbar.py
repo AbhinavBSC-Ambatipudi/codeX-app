00997ee91614981be7e6e72614f4002367cb461b
ABHINAV B S C AMBATIPUDI <168204964+AbhinavBSC-Ambatipudi@users.noreply.github.com>
2025-04-18 21:44:00 +0530

    Update navbar.py

--- components/navbar.py
+++ components/navbar.py
@@ -1,7 +1,6 @@
 import streamlit as st
 
 def navbar():
-    # Custom CSS for the navbar
     st.markdown("""
     <style>
     .navbar {
@@ -45,8 +44,7 @@ def navbar():
     }
     </style>
     """, unsafe_allow_html=True)
-    
-    # Navbar HTML
+
     st.markdown("""
         <div class="navbar">
             <div class="nav-container">
@@ -56,28 +54,34 @@ def navbar():
                 <div class="nav-right">
     """, unsafe_allow_html=True)
 
-    # Navigation buttons
+    # Navigation logic with a rerun flag
+    rerun_required = False
+
     col1, col2, col3, col4 = st.columns([1, 1, 1, 1])
-    
+
     with col1:
         if st.button("Home", key="nav_home"):
             st.session_state.current_page = "home"
-            st.rerun()
-    
+            rerun_required = True
+
     with col2:
         if st.button("Code Editor", key="nav_editor"):
             st.session_state.current_page = "code_editor"
-            st.rerun()
-    
+            rerun_required = True
+
     with col3:
         if st.button("Chat", key="nav_chat"):
             st.session_state.current_page = "chatbot"
-            st.rerun()
-    
+            rerun_required = True
+
     with col4:
         if st.button("Logout", key="nav_logout"):
             for key in list(st.session_state.keys()):
                 del st.session_state[key]
-            st.rerun()
+            rerun_required = True
 
-    st.markdown("</div></div></div>", unsafe_allow_html=True) 
+    st.markdown("</div></div></div>", unsafe_allow_html=True)
+
+    # Rerun once, safely
+    if rerun_required:
+        st.rerun()
